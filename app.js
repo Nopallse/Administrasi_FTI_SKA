1caaa31692ff5b4faf98a12b0aca7bb7fb812496
nopallse <noppal.901@gmail.com>
2024-05-06 14:12:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+app.use(express.static('src'));
+
+
+// Mengatur rute untuk halaman utama
+app.get('/', (req, res) => {
+  res.send("hello");
+});
+
+// Menjalankan server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
